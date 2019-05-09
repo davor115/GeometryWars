f66f760369a1edf69f0de611a616a79ab3042b5d
Davor Larsen <s5008226@bournemouth.ac.uk>
2019-05-09 16:50:19 +0100

    Fix the boss bullet script...

--- Assets/Scripts/Player/Boss_BulletVelocity.cs
+++ Assets/Scripts/Player/Boss_BulletVelocity.cs
@@ -1,15 +1,20 @@
 ﻿// Rory Clark - https://rory.games - 2019
 using UnityEngine;
 
-public class BulletVelocity : MonoBehaviour
+public class Boss_BulletVelocity : MonoBehaviour
 {
     Rigidbody m_rigidbody;
-
+    GameObject Player;
     [SerializeField]
     float m_maxVelocity = 100f;
 
     [SerializeField]
-    float m_destroyDistance = 150f;
+    float m_destroyDistance = 20.0f;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     private void Awake()
     {
@@ -18,17 +23,16 @@ public class BulletVelocity : MonoBehaviour
 
     void Update()
     {
-        
+
         // if (m_rigidbody.velocity.magnitude > m_maxVelocity)
         // {
         //    m_rigidbody.velocity = Vector3.ClampMagnitude(m_rigidbody.velocity, m_maxVelocity);
         // }
-       
-            gameObject.transform.Translate(transform.forward * 50 * Time.deltaTime);     
+            gameObject.transform.Translate(new Vector3(0, 0, 1) * 50 * Time.deltaTime);     
       
         // Destroy the object if it's gone to far
         // Much easier than checking if it's directly off the screen, however has to be manually defined and will not work effectively on different aspect ratios
-        if (transform.position.x >= Mathf.Abs(m_destroyDistance))
+        if (transform.position.x <= Mathf.Abs(m_destroyDistance))
         {
             Destroy(gameObject);
         }
